a83ca3f5ce24b079b23caccd627b809423881483
KratiGupta <kratigupta8545@gmail.com>
2021-07-23 21:19:43 +0530

    Added switch case successfully

--- EmployeeWageBuild.java
+++ EmployeeWageBuild.java
@@ -3,29 +3,35 @@ import java.lang.Math;
 
 public class EmployeeWageBuild {
 	
+	public static final  int salaryPerHour=20,partTime=1,fullTime=2;
+	
 		   public static void main(String[] args) {
 			    System.out.println("Welcome to Salary Calculator on Master branch"); 
-			    
-			   int salaryPerHour=20,partTime=1,fullTime=2;
+			   
 			   int empHours=0,empWageDaily=0;
 			    
-			  double empCheck = Math.floor(Math.random() *10 )% 3;
-			  if(empCheck==partTime) {
+			  int empCheck = (int)Math.floor(Math.random() *10 )% 3;
+			  
+			  switch(empCheck) {
+			  
+			      case partTime:
 				  System.out.println("Employee is Present Part Time");
 				  empHours=4;
-			  }
-			  
-			  else if(empCheck==fullTime) {
+				  break;
 				  
-				 System.out.println("Employee is Present Full Time");
-				 empHours=8;
+			      case fullTime:
+			    	  System.out.println("Employee is Present Full Time");
+			    	  empHours=8;
+			    	  break;
+			    	  
+			      default:
+			    	  System.out.println("Employee is Absent");
+			    	  empHours=0;
+			    	  
 			  }
-			  
-			  else {
-				  
-				  System.out.println("Employee is Absent");
-				  
-			  }
+			    	  
+			    
+		
 			  empWageDaily = empHours*salaryPerHour;
 			  System.out.println("Daily Wage Of Employee is: " + empWageDaily);
 			  
