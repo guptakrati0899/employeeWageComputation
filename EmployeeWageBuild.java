ae1fd2fe2984cbf0d29133a0a9cbe7ab345dbd76
KratiGupta <kratigupta8545@gmail.com>
2021-07-23 20:41:08 +0530

    Added Parttime employee successfully

--- EmployeeWageBuild.java
+++ EmployeeWageBuild.java
@@ -6,17 +6,25 @@ public class EmployeeWageBuild {
 		   public static void main(String[] args) {
 			    System.out.println("Welcome to Salary Calculator on Master branch"); 
 			    
-			   int empHours=0,salaryPerHour=20,empWageDaily=0;
+			   int salaryPerHour=20,partTime=1,fullTime=2;
+			   int empHours=0,empWageDaily=0;
 			    
-			  double empCheck = Math.floor(Math.random() *10 )% 2;
-			  if(empCheck==1) {
-				  System.out.println("Employee is Present");
-				  empHours=8;
+			  double empCheck = Math.floor(Math.random() *10 )% 3;
+			  if(empCheck==partTime) {
+				  System.out.println("Employee is Present Part Time");
+				  empHours=4;
 			  }
+			  
+			  else if(empCheck==fullTime) {
+				  
+				 System.out.println("Employee is Present Full Time");
+				 empHours=8;
+			  }
+			  
 			  else {
 				  
-				 System.out.println("Employee is absent");
-				 empHours=0;
+				  System.out.println("Employee is Absent");
+				  
 			  }
 			  empWageDaily = empHours*salaryPerHour;
 			  System.out.println("Daily Wage Of Employee is: " + empWageDaily);
