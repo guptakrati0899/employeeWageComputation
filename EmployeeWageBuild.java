dd13bdcd6f6f7ffa182647505cc3eb05f1055ff0
KratiGupta <kratigupta8545@gmail.com>
2021-07-23 19:42:12 +0530

    Added welcome message successfully

---
+++ EmployeeWageBuild.java
@@ -0,0 +1,12 @@
+package employeeWageComputation;
+
+public class EmployeeWageBuild {
+	
+		   public static void main(String[] args) {
+			    System.out.println("Welcome to Salary Calculator on Master branch"); 
+		
+				    }
+				           			    
+
+	}
+
