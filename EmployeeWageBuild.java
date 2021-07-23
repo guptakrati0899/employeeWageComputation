4891abf274b6455e7f1e0a71c28607cfbf939d4a
KratiGupta <kratigupta8545@gmail.com>
2021-07-23 20:21:42 +0530

    Added Employee daily wage computation

--- EmployeeWageBuild.java
+++ EmployeeWageBuild.java
@@ -6,14 +6,23 @@ public class EmployeeWageBuild {
 		   public static void main(String[] args) {
 			    System.out.println("Welcome to Salary Calculator on Master branch"); 
 			    
+			   int empHours=0,salaryPerHour=20,empWageDaily=0;
+			    
 			  double empCheck = Math.floor(Math.random() *10 )% 2;
 			  if(empCheck==1) {
 				  System.out.println("Employee is Present");
+				  empHours=8;
 			  }
 			  else {
 				  
 				 System.out.println("Employee is absent");
+				 empHours=0;
 			  }
+			  empWageDaily = empHours*salaryPerHour;
+			  System.out.println("Daily Wage Of Employee is: " + empWageDaily);
+			  
+			  
+			  
 		
 		  }
 				           			    
