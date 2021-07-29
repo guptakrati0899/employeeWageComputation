c725a1653d2bd2a86a5e36fdb5c53246714562ee
KratiGupta <kratigupta8545@gmail.com>
2021-07-30 00:25:13 +0530

    Refactored Employee wage using Class Methods

--- EmployeeWageBuild.java
+++ EmployeeWageBuild.java
@@ -5,43 +5,58 @@ public class EmployeeWageBuild {
 	
 	public static final  int salaryPerHour=20,partTime=1,fullTime=2,workingDays=10,maxWorkingHours=60;
 	
-		   public static void main(String[] args) {
-			    System.out.println("Welcome to Salary Calculator on Master branch"); 
+	       public static int computeEmployeeWage() {
+	    	   
+	    	   int empHours=0,totalEmpHours=0,totalWorkingDays=0,totalEmpWage=0;
+	    	   
+	    	   while(totalWorkingDays < workingDays && totalEmpHours < maxWorkingHours) {
+	    		   
+	    		   totalWorkingDays ++;
+				    
+					  int empCheck = (int)Math.floor(Math.random() *10 )% 3;
+					  
+					  
+					  switch(empCheck) {
+					  
+					      case partTime:
+						  System.out.println("Employee is Present Part Time");
+						  empHours=4;
+						  break;
+						  
+					      case fullTime:
+					    	  System.out.println("Employee is Present Full Time");
+					    	  empHours=8;
+					    	  break;
+					    	  
+					      default:
+					    	  System.out.println("Employee is Absent");
+					    	  empHours=0;
+					    	  
+					  }
+					  
+					  
+
+					    totalEmpHours += empHours;
+					    System.out.println("DAY:"+ totalWorkingDays +" Emphrs:" + totalEmpHours);
+					    	  
+			    }
+	    	   
+	    	   
+					    totalEmpWage= totalEmpHours * salaryPerHour;
+					  
+					    System.out.println("Total wage is: " + totalEmpWage);
+					    return totalEmpWage;
+					 	  
+				
+	       }
+					  	
+	    
+	
+		        public static void main(String[] args) {
+			       System.out.println("Welcome to Salary Calculator on Master branch"); 
 			   
-			   int empHours=0,totalEmpHours=0,totalWorkingDays=0,totalEmpWage=0;
-			   
-			  while(totalWorkingDays < workingDays && totalEmpHours < maxWorkingHours) {
-				  totalWorkingDays ++;
-			    
-			  int empCheck = (int)Math.floor(Math.random() *10 )% 3;
-			  
-			  
-			  switch(empCheck) {
-			  
-			      case partTime:
-				  System.out.println("Employee is Present Part Time");
-				  empHours=4;
-				  break;
-				  
-			      case fullTime:
-			    	  System.out.println("Employee is Present Full Time");
-			    	  empHours=8;
-			    	  break;
-			    	  
-			      default:
-			    	  System.out.println("Employee is Absent");
-			    	  empHours=0;
-			    	  
-			  }
-			  
-			  totalEmpHours += empHours;
-			  System.out.println("DAY:"+ totalWorkingDays +"Emp hrs:" + totalEmpHours);
-			    	  
-			  }
-			  totalEmpWage= totalEmpHours * salaryPerHour;
-			  
-			  System.out.println("Total wage is: " + totalEmpWage);
-			 	  
+			      computeEmployeeWage();
+			
 		
 		  }
 				           			    
