7896b300529f0ccd4c4571f43675bbe7395368ae
KratiGupta <kratigupta8545@gmail.com>
2021-08-01 00:25:16 +0530

    Refactored the Employee save code

--- EmployeeWageBuild.java
+++ EmployeeWageBuild.java
@@ -1,93 +1,74 @@
 package employeeWageComputation;
+import java.lang.Math;
 
 
 public class EmployeeWageBuild {
 	
- public static final  int partTime=1,fullTime=2;
- private final String company;
- private final int salaryPerHour;
- private final int workingDays;
- private final int maxWorkingHours;
- private int totalEmpWage;
- 
- 
- public EmployeeWageBuild (String company ,int salaryPerHour, int workingDays, int maxWorkingHours) {
-	 
-	 this.company = company;
-	 this.salaryPerHour = salaryPerHour;
-	 this.workingDays = workingDays;
-	 this.maxWorkingHours = maxWorkingHours;
-	 
- }
-	
-public void computeEmployeeWage() {
-	    	   
-	    	   int empHours=0,totalEmpHours=0,totalWorkingDays=0;
-	    	   
-	    	   while(totalWorkingDays < workingDays && totalEmpHours < maxWorkingHours) {
-	    		   
-	    		   totalWorkingDays ++;
-				    
-					  int empCheck = (int)Math.floor(Math.random() *10 )% 3;
-					  
-					  
-					  switch(empCheck) {
-					  
-					      case partTime:
-						  System.out.println("Employee is Present Part Time");
-						  empHours=4;
-						  break;
-						  
-					      case fullTime:
-					    	  System.out.println("Employee is Present Full Time");
-					    	  empHours=8;
-					    	  break;
-					    	  
-					      default:
-					    	  System.out.println("Employee is Absent");
-					    	  empHours=0;
-					    	  
-					  }
-					  
-					  
 
-					    totalEmpHours += empHours;
-					    System.out.println("DAY:"+ totalWorkingDays +" Emphrs:" + totalEmpHours);
-					    	  
-			    }
-	    	   
-	    	   
-					    totalEmpWage= totalEmpHours * salaryPerHour;
-					     System.out.println("The wage for "+ company + " is "+ totalEmpWage); 
-					     
-					     int totalWageTCS=5*2*10;
-					     int totalWageAccenture=3*4*20;
-					     
-					     int wageSaveTCS=totalWageTCS-totalEmpWage;
-					 
-				
-	       }
-                    
-                      
-             
-              
-              
-					  	
+
+	    public static final int PARTTIME = 1;
+	    public static final int FULLTIME = 2;
+
+	    private final String company;
+	    private final int empRatePerHour;
+	    private final int numberOfWorkingDays;
+	    private final int hrsPerMonth;
+	    private int totalEmpWage;    
 	    
-	
-		        public static void main(String[] args) {
-			       System.out.println("Welcome to Salary Calculator on Master branch"); 
-			       
-			      EmployeeWageBuild TCS =new EmployeeWageBuild("TCS",5,2,10);
-			      EmployeeWageBuild Accenture =new EmployeeWageBuild("Accenture",3,4,20);
-			      
-			      TCS.computeEmployeeWage();
-			      System.out.println(TCS);
-			      Accenture.computeEmployeeWage();
-			      System.out.println(Accenture);
-		
-		  }
-				           			    
+	    public EmployeeWageBuild(String company, int empRatePerHour, int numberOfWorkingDays, 
+	                                            int hrsPerMonth) {
+	        
+	        this.company = company;
+	        this.empRatePerHour = empRatePerHour;
+	        this.numberOfWorkingDays = numberOfWorkingDays;
+	        this.hrsPerMonth = hrsPerMonth;
 
+	    }
+
+	    public void empWageComputation() {
+	        
+	        //Variables
+	        int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+
+	        //Computation
+	        while (totalEmpHrs <= hrsPerMonth &&
+	               totalWorkingDays < numberOfWorkingDays) {
+	            totalWorkingDays++;
+	            int checkEmp = (int) Math.floor(Math.random() * 10) % 3;
+	            switch (checkEmp) {
+	                case FULLTIME :
+	                    empHrs = 8;
+	                    break;
+	                case PARTTIME :
+	                    empHrs = 4;
+	                    break;
+	                default :
+	                    empHrs = 0;
+	            }
+	            totalEmpHrs += empHrs;
+	            System.out.println("Employee Working Days : " +totalWorkingDays + 
+	                                "     Employee Working Hours : " +totalEmpHrs);
+	        }
+	        totalEmpWage = totalEmpHrs + empRatePerHour;
+
+	    }
+
+	    @Override
+	    public String toString() {
+	        return "Total Employee Wage for " +company+ " Company is : " +totalEmpWage;
+	    }
+	    public static void main (String[] args) {
+	    	EmployeeWageBuild TCS = new EmployeeWageBuild("TCS", 20, 1, 10);
+	    	EmployeeWageBuild Accenture = new EmployeeWageBuild("Accenture", 10, 2, 20);
+	    	EmployeeWageBuild HCL = new EmployeeWageBuild("HCL", 10, 3, 20);
+	    	EmployeeWageBuild Magic = new EmployeeWageBuild("Magic edTech", 20, 4, 10);
+	        TCS.empWageComputation();
+	        System.out.println(TCS);
+	        Accenture.empWageComputation();
+	        System.out.println(Accenture);
+	        HCL.empWageComputation();
+	        System.out.println(HCL);
+	        Magic.empWageComputation();
+	        System.out.println(Magic);
+	    }
 	}
-
